a627a73cf1d54b75dcc4f872024faba69389adbd
Beau <beau@beaumartinez.com>
2012-09-17 23:18:13 +0100

    Add about view

--- home/views.py
+++ home/views.py
@@ -1,10 +1,25 @@
+from datetime import date, datetime
+
+from dateutil.relativedelta import relativedelta
 from flask import render_template, request
 
 from home import app
 
+BIRTHDAY = date(1990, 4, 4)
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/about/')
+def about():
+    now = datetime.utcnow()
+    now = now.date()
+
+    age = relativedelta(now, BIRTHDAY)
+    age = age.years
+
+    return render_template('about.html', age=age)
 
 @app.errorhandler(404)
 def page_not_found(error):
