80a2bd0daaac23dbbd75f11475737301701bf81d
Beau <beau@beaumartinez.com>
2012-09-23 11:06:06 +0100

    Add 500 view

--- home/views.py
+++ home/views.py
@@ -30,3 +30,7 @@ def page_not_found(error):
     path = path.strip('/')
 
     return render_template('404.html', path=path), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
