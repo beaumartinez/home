db0967b3ef681c7803f02e532aba530cb688f806
Beau <beau@beaumartinez.com>
2013-02-07 19:53:34 +0000

    Rejig web font loading logic, add fallback

--- home/static/js/webfonts.js
+++ home/static/js/webfonts.js
@@ -4,11 +4,17 @@
     window.WebFontConfig = {
         google: {
             families: ['Open+Sans:300,400:latin'],
-        }
+        },
+
+        active: function() {
+            document.body.parentNode.classList.remove('wf-loading');
+        },
     };
 
     var script = document.createElement('script');
     script.src = '//ajax.googleapis.com/ajax/libs/webfont/1.1.2/webfont.js';
     
     document.body.appendChild(script);
+    
+    window.setTimeout(window.WebFontConfig.active, 1000);
 })();
